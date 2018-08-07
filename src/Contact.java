5ac7ad0b8c0cf1880efbc8e5eaa301160313464b
Jillian Wysoki <jillianwysoki@gmail.com>
2018-08-07 17:43:36 -0500

    developed contacts.txt

---
+++ src/Contact.java
@@ -0,0 +1,15 @@
+public class Contact extends ReadWrite {
+
+//public static readWrite = new ReadWrite();
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
--- src/ReadWrite.java
+++ src/ReadWrite.java
@@ -6,10 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadWrite {
+
+
     private List<String> readString;
     private String directory;
     private String filename;
     private Path listOfContactsPath = Paths.get(directory,filename );
+
+    public ReadWrite(String directory, String filename) {
+        this.directory = directory;
+        this.filename = filename;
+    }
 
     public ReadWrite(){}
 
@@ -22,15 +29,31 @@ public class ReadWrite {
         return readString;
     }
 
-    public void WriteContacts(ArrayList<String> contacts){
+    public void WriteContacts(ArrayList<String> contact){
         try {
-            Files.write(listOfContactsPath, contacts);
+            Files.write(listOfContactsPath, contact);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-//    public void AddContact(String contact){
+    public List<String> getReadString() {
+        return readString;
+    }
+
+    public void setReadString(List<String> readString) {
+        this.readString = readString;
+    }
+
+    public Path getListOfContactsPath() {
+        return listOfContactsPath;
+    }
+
+    public void setListOfContactsPath(Path listOfContactsPath) {
+        this.listOfContactsPath = listOfContactsPath;
+    }
+
+    //    public void AddContact(String contact){
 //        try {
 //            Files.write(listOfContactsPath, contact, StandardOpenOption.APPEND);
 //        } catch (Exception e){
@@ -52,5 +75,14 @@ public class ReadWrite {
 
     public void setFilename(String filename) {
         this.filename = filename;
+    }
+
+    public static void main(String[] args) {
+        ReadWrite readWrite = new ReadWrite("../data", "test.txt");
+        readWrite.setDirectory("../data");
+        readWrite.setFilename("test.txt");
+        System.out.println(readWrite.getFilename());
+        Path testPath = Paths.get("../data", "test.txt");
+        System.out.println(Files.exists(testPath));
     }
 }
