71d060cf288e5c3dc5f0e2e66cde35fa1d4cd270
Jillian Wysoki <jillianwysoki@gmail.com>
2018-08-08 10:39:41 -0500

    getters, setters, constructors for Contact.java

--- src/Contact.java
+++ src/Contact.java
@@ -1,15 +1,51 @@
+import java.util.Scanner;
+
 public class Contact{
+    private Scanner sc = new Scanner(System.in);
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
 
-//public static readWrite = new ReadWrite();
+    public Contact(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
 
 
-
-
-
-    public static void main(String[] args) {
+    public Contact() {
+        System.out.println("Please enter a first name");
+        this.firstName = sc.nextLine();
+        System.out.println("Please enter a last name");
+        this.lastName = sc.nextLine();
+        System.out.println("Please enter a ten-digit phone number");
+        this.phoneNumber = sc.nextLine();
 
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
 
 
